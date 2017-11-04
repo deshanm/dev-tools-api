56541adb7d4e1fd4c3eef237bb26fd114b961cdf
deshatom <deshatom@gmail.com>
2017-11-04 17:19:18 +0530

    removed some files and updated package.json

--- db_backup/build copy.js
+++
@@ -1,7 +0,0 @@
-const s = require('shelljs');
-
-s.rm('-rf', 'build');
-s.mkdir('build');
-s.cp('-R', 'public', 'build/public');
-s.mkdir('-p', 'build/server/swagger');
-s.cp('server/swagger/Api.yaml', 'build/server/swagger/Api.yaml');
